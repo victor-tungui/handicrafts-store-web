7b9d307c6d41616aef75768ca58bb3fd2b3382ca
Victor H Tungui <capishy@gmail.com>
2020-05-25 20:11:25 -0500

    Adding boostrap library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Button } from 'reactstrap'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, welcome to the store</h1>
+
+      <Button color="primary">Button from boostrap</Button>
     </div>
+
+    
   );
 }
 
